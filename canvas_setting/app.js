664964c3bc5c755b7435f1bd7128e135e9392990
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-07-09 12:41:13 +0900

    Create app.js

---
+++ canvas_setting/app.js
@@ -0,0 +1,36 @@
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas); 
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        // 윈도우 창 크기가 변할 때마다 resize 호출
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    // 캔버스 사이즈 지정
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+    }
+
+    // 그림 그리는 함수
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    }
+}
+
+window.onload = () => {
+    new App();
+}
