e87f7d76cb0a8c20538bf43bcea90619051f7371
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-07-16 00:00:05 +0900

    Update sheepController.js

--- sheep/sheepController.js
+++ sheep/sheepController.js
@@ -1,7 +1,15 @@
 import {Sheep} from "./sheep.js";
 
-export class SheepController {
-    constructor() {
+/*
+같은 객체이지만 따로따로 관리가 필요한 경우 Controller로 제어
+    - 각 객체 출력
+    - 화면에서 넘어가면 삭제
+*/
+
+export class SheepController 
+{
+    constructor() 
+    {
         this.img = new Image();
         this.img.onload = () => {
             this.loaded();
@@ -14,43 +22,46 @@ export class SheepController {
         this.isLoaded = false;
     }
 
-    resize(stageWidth, stageHeight) {
+    resize(stageWidth, stageHeight) 
+    {
         this.stageWidth = stageWidth;
         this.stageHeight = stageHeight;
     }
 
-    loaded() {
+    loaded() 
+    {
         this.isLoaded = true;
         this.addSheep();
     }
 
-    addSheep() {
+    addSheep() 
+    {
         this.items.push(
             new Sheep(this.img, this.stageWidth),
         );
     }
 
-    draw(ctx, t, dots) {
+    draw(ctx, t, dots) 
+    {
         if(this.isLoaded) {
             this.cur += 1;
-            if(this.cur > 200) {
+            if(this.cur > 200) 
+            {
                 this.cur = 0;
                 this.addSheep();
             }
 
             // 양들을 순회하면서 출력
-            for(let i=this.items.length-1; i>=0; i--) {
+            for(let i=this.items.length-1; i>=0; i--) 
+            {
                 const item = this.items[i];
 
                 // 화면에서 넘어가면 배열에서 삭제
-                if(this.items.x < -item.width) {
+                if(this.items.x < -item.width)
                     this.items.splice(i, 1);
-                } 
                 
                 // 출력
-                else {
-                    item.draw(ctx, t, dots);
-                }
+                else item.draw(ctx, t, dots);
             }
             
         }
