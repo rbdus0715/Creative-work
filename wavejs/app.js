c4be4dd1b5862c6fde08665511c99ba99e48bd7e
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-07-23 10:32:33 +0900

    Update app.js

--- wavejs/app.js
+++ wavejs/app.js
@@ -1,44 +1,42 @@
-import { Wave } from "./wave";
+import { WaveGroup } from './wavegroup.js';
 
 class App {
-    constructor() {
-        this.canvas = document.createElement('canvas');
-        document.body.appendChild(this.canvas); 
-        this.ctx = this.canvas.getContext('2d');
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
 
-        this.wave = new Wave();
+    this.WaveGroup = new WaveGroup();
 
-        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+    // 윈도우 창 크기가 변할 때마다 resize 호출
+    window.addEventListener('resize', this.resize.bind(this), false);
+    this.resize();
 
-        // 윈도우 창 크기가 변할 때마다 resize 호출
-        window.addEventListener('resize', this.resize.bind(this), false);
-        this.resize();
+    requestAnimationFrame(this.animate.bind(this));
+  }
 
-        window.requestAnimationFrame(this.animate.bind(this));
-    }
+  // 캔버스 사이즈 지정
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
 
-    // 캔버스 사이즈 지정
-    resize() {
-        this.stageWidth = document.body.clientWidth;
-        this.stageHeight = document.body.clientHeight;
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
 
-        this.canvas.width = this.stageWidth * this.pixelRatio;
-        this.canvas.height = this.stageHeight * this.pixelRatio;
-        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+    this.WaveGroup.resize(this.stageWidth, this.stageHeight);
+  }
 
-        this.wave.resize();
-    }
+  // 그림 그리는 함수
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
 
-    // 그림 그리는 함수
-    animate() {
-        window.requestAnimationFrame(this.animate.bind(this));
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
-        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-
-        this.wave.draw();
-    }
+    this.WaveGroup.draw(this.ctx);
+  }
 }
 
 window.onload = () => {
-    new App();
-}
+  new App();
+};
