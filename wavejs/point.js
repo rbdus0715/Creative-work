006ac15acb0fec7315a2a395dcca0ce5edc6ecbc
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-07-23 10:32:45 +0900

    Update point.js

--- wavejs/point.js
+++ wavejs/point.js
@@ -1,15 +1,15 @@
 export class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.fixedY = y;
-        this.speed = 0.1;
-        this.cur = 0; // sin(x) 에서 x
-        this.max = Math.random() * 100 + 150 // 사인함수의 계수
-    }
+  constructor(index, x, y) {
+    this.x = x;
+    this.y = y;
+    this.fixedY = y;
+    this.speed = 0.1;
+    this.cur = index; // sin(x) 에서 x
+    this.max = Math.random() * 100 + 150; // 사인함수의 계수
+  }
 
-    update() {
-        this.cur += this.speed;
-        this.y = this.fixedY + (Math.sin(this.cur) * this.max);
-    }
+  update() {
+    this.cur += this.speed;
+    this.y = this.fixedY + Math.sin(this.cur) * this.max;
+  }
 }
